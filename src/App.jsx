16650487e0fcbc2acf1179356222bbb743af8544
Matt Wong <matthew067d.wong@libertymutual.com>
2019-03-28 17:28:13 -0400

    fetch and show data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import KudosCard from 'KudosCard';
 
-const App = () => (
-  <KudosCard
-    src="https://avatars.slack-edge.com/2019-03-28/593216061622_b2e2e2572d29e3eeb66b_192.png"
-    author="Juan"
-    recipient="Matt Wong"
-    message={
-      <>
-        Alan Rails, ladies and gentlemen. After his parents' tragic death in a
-        railroad accident, he gained the power to summon ghost trains. It's not
-        all bad though, they were spared having to see their grown son wear a
-        whistle. Yeah, sure, I mean, if you spend all day shuffling words
-        around, you can make anything sound bad. This isn't Game of Thrones,
-        Morty. Aww, gee, you got me there, Rick.
-      </>
-    }
-  />
-);
+const getMessages = async (kudos, setKudos) => {
+  if (kudos.length === 0) {
+    const response = await fetch('/messages');
+    const messages = await response.json();
+
+    setKudos(messages);
+  }
+};
+
+const App = () => {
+  const [kudos, setKudos] = useState([]);
+  const kudo = kudos[0];
+
+  useEffect(() => {
+    getMessages(kudos, setKudos);
+  }, [kudos]);
+
+  return kudo ? (
+    <KudosCard
+      src={kudo.recipient.image}
+      author={kudo.author.name}
+      recipient={kudo.recipient.name}
+      message={kudo.text}
+    />
+  ) : null;
+};
 
 export default App;
