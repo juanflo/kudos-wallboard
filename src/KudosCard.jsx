143003de3ec0799cf1c2ceb9b3035edb668d30f1
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2019-04-08 14:05:49 -0400

    update style

--- src/KudosCard.jsx
+++ src/KudosCard.jsx
@@ -3,33 +3,40 @@ import PropTypes from 'prop-types';
 import { Card, Image } from 'semantic-ui-react';
 
 const KudosCard = ({ src, recipients, author, message, reactions }) => (
-  <Card fluid>
-    <div className="kudos">
-      <Card.Content>
-        <Image floated="left" src={src} size="small" />
-        <Card.Header>
-          To:&nbsp;
-          {recipients.map(recipient => {
+  <>
+    <Card fluid>
+      <div className="kudos">
+        <Card.Content>
+          <Image floated="left" src={src} size="small" />
+          <Card.Header>
+            <div className="name-label">To:&nbsp;</div>
+            <div className="name">
+              {recipients.map(recipient => {
             return recipient.name;
-        }).join(', ')}
-        </Card.Header>
-        <Card.Meta>
-          From:&nbsp;
-          {author}
-        </Card.Meta>
-        <Card.Description>{message}</Card.Description>
-        {reactions && reactions.length > 0 ?
+            }).join(', ')}
+              {' '}
+            </div>
+          </Card.Header>
+          <Card.Meta>
+            <div className="name-label">From:&nbsp;</div>
+            <div className="name"> 
+              {author}
+            </div>
+          </Card.Meta>
+          <Card.Description>{message}</Card.Description>
+          {reactions && reactions.length > 0 ? (
             <Card.Description>
               {
                 <Card.Group className="all-reactions">
-                  {reactions.map(reaction => <div className="reaction">{`${reaction.emoji} ${reaction.count}`}</div>)}
+                  {reactions.map(reaction => <div className="reaction" key={reaction.emoji}>{`${reaction.emoji} ${reaction.count}`}</div>)}
                 </Card.Group>
               }
             </Card.Description>
-            : null}
-      </Card.Content>
-    </div>
-  </Card>
+): null}
+        </Card.Content>
+      </div>
+    </Card>
+  </>
 );
 
 KudosCard.propTypes = {
